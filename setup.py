9f25a69426c022a9698137bf717a6b82c4d2089e
vubon <vubon.roy@gmail.com>
2020-05-03 12:24:39 +0600

    modified package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="PyCronExpression-vubon",  # Replace with your own username
+    name="PyCronExpression",
     version="0.0.1",
     author="Vubon Roy",
     author_email="vubon.roy@gmail.com",
