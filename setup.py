bc04b2d8ec9e7f46da08fca77308da805ed2576c
vubon <vubon.roy@gmail.com>
2020-05-28 13:26:39 +0600

    added CHANGELOG.md in setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+def get_long_description():
+    with open("README.md", "r") as readme:
+        with open("CHANGELOG.md", "r") as changelog:
+            return readme.read() + "\n\n" + changelog.read()
+
 
 setuptools.setup(
     name="PyCronExpression",
-    version="0.0.7",
+    version="0.1.0",
     author="Vubon Roy",
     author_email="vubon.roy@gmail.com",
     description="A simple Python Cron Expression lib",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
     url="https://github.com/vubon/PyCronExpression.git",
     packages=setuptools.find_packages(),
@@ -17,10 +21,11 @@ setuptools.setup(
         'pytz',
         'tzlocal'
     ],
+    python_requires='>=3.6',
+    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    ]
 )
